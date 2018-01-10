cad4f644633ed151f9ced37b9745686f9ed90b51
xianjing <zhuoxianjing@gmail.com>
2018-01-10 15:04:23 +0800

    add order test demo

---
+++ src/test/java/com/dmall/demo/DemoTest.java
@@ -0,0 +1,15 @@
+package com.dmall.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by xianjing on 10/01/2018.
+ */
+public class DemoTest {
+    @Test
+    public void should_return_true(){
+        assertEquals(true, true);
+    }
+}
---
+++ src/test/java/com/dmall/demo/order/OrderTest.java
@@ -0,0 +1,19 @@
+package com.dmall.demo.order;
+
+import org.junit.Test;
+
+/**
+ * Created by xianjing on 10/01/2018.
+ */
+public class OrderTest {
+
+//    @Test
+//    public void should_create_order(){
+//
+//        //create order
+//        //order save into repository
+//        //actualOrder = repository.getById(...)
+//        //assertSame(order, actualOrder)
+//    }
+
+}
